a7e6ac0291f4a1f1e85a4d3a31303d638f3e9ad3
SySyAli <syedansabali786@gmail.com>
2024-11-01 01:21:20 -0500

    Removing some padding.

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 
 const Header = () => (
-	<h1 className="text-2xl font-bold text-center my-4">
+	<h1 className="text-2xl font-bold text-center">
 		Vanderbilt MoveVU Ridership Dashboard
 	</h1>
 );
